0605ae17abaa2006033b482012138cdd0e10ea9f
Ralph Stout <150555660+CodeByRalph@users.noreply.github.com>
2025-06-05 19:21:40 -0500

    Fix CSS syntax error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -86,6 +86,8 @@ const config: Config = {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'float': 'float 6s ease-in-out infinite',
+        'float-delayed': 'float 6s ease-in-out infinite 3s',
         'fade-in': 'fade-in 0.3s ease-out',
       },
     },
